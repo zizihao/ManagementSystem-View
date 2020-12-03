887af9e7f30960af102f85454b5fb2bfec47f50c
chenzihao <chenzihao_2020@163.com>
2020-12-03 21:03:26 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugin/element'
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
 
 Vue.config.productionTip = false
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="container">
     <div class="main">
+      <div class="logo">
+        <img src="../assets/logo.png" alt="" />
+      </div>
       <div class="import">
         <div class="import-box">
           <p>用户名：</p>
           <div class="input-box">
-            <el-input v-model="input" placeholder="请输入内容"></el-input>
+            <el-input v-model="username" placeholder="请输入内容"></el-input>
           </div>
         </div>
         <div class="import-box">
           <p>密码：</p>
           <div class="input-box">
-            <el-input v-model="input" placeholder="请输入内容"></el-input>
+            <el-input v-model="password" placeholder="请输入内容"></el-input>
           </div>
         </div>
       </div>
       <div class="btn">
-        <el-button type="primary" class="login" round>登录</el-button>
-        <el-button type="info" class="reset" round>重置</el-button>
+        <el-button type="primary" class="login" round @click="onSumber"
+          >登录</el-button
+        >
+        <el-button type="info" class="reset" round @click="reset"
+          >重置</el-button
+        >
       </div>
     </div>
   </div>
@@ -27,8 +34,27 @@
 export default {
   data() {
     return {
-      input: "",
+      username: "",
+      password: "",
     };
+  },
+  methods: {
+    onSumber() {
+      this.$axios({
+        method: "post",
+        url: "/login",
+        data: {
+          username: this.username,
+          password: this.password,
+        },
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+    reset() {
+      this.username = "";
+      this.password = "";
+    },
   },
 };
 </script>
@@ -42,13 +68,34 @@ export default {
   height: 100%;
 }
 .main {
+  position: relative;
   display: flex;
   flex-direction: column;
-  width: 450px;
-  height: 300px;
+  width: 500px;
+  height: 350px;
   border-radius: 3px;
   background-color: #fff;
+  .logo {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+      object-fit: cover;
+    }
+  }
   .import {
+    margin-top: 65px;
     flex: 3;
     display: flex;
     flex-direction: column;
@@ -56,7 +103,7 @@ export default {
     align-items: center;
     .import-box {
       display: flex;
-      width: 85%;
+      width: 80%;
       p {
         flex: 1;
         display: flex;
@@ -73,6 +120,7 @@ export default {
     display: flex;
     justify-content: space-evenly;
     align-items: flex-start;
+    margin-bottom: 9px;
     .login,
     .reset {
       margin: 0;
